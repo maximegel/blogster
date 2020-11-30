ff4bc2f898b0480db783eda96f676507cd4a07a4
Maxime Gélinas <mgelinas@softframe.net>
2020-11-30 17:09:46 -0500

    chore: edit commitlint scopes

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,6 +3,6 @@ const { getPackages } = require('@commitlint/config-lerna-scopes').utils;
 module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
   rules: {
-    'scope-enum': async ctx => [2, 'always', [...(await getPackages(ctx)), '*', 'release', 'readme']],
+    'scope-enum': async ctx => [2, 'always', [...(await getPackages(ctx)), '*', 'readme', 'release', 'vscode']],
   },
 };
