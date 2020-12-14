5a0f3032f8ba54ef24cc4091ccd46cb6e3095fbb
Maxime Gélinas <mgelinas@softframe.net>
2020-12-14 15:51:40 -0500

    fix(core): resolve config plugins and globs relative to config file

--- packages/core/src/config/providers/cosmic.ts
+++ packages/core/src/config/providers/cosmic.ts
@@ -1,5 +1,23 @@
 import { cosmiconfig } from 'cosmiconfig';
+import findRoot from 'find-root';
+import path from 'path';
 import { ConfigProvider, createConfigProvider } from '../config-provider';
 
-export const cosmic = (moduleName: string): ConfigProvider =>
-  createConfigProvider(async () => (await (await cosmiconfig(moduleName).search())?.config) ?? {});
+export const cosmic = (moduleName: string, file?: string): ConfigProvider =>
+  createConfigProvider(async () => {
+    const cosmic = cosmiconfig(moduleName);
+    const { config, filepath } = file ? await cosmic.load(file) : await cosmic.search();
+    if (!config) return {};
+    const root = findRoot(process.cwd());
+    const configDir = path.dirname(filepath);
+    return {
+      ...config,
+      // Makes plugin paths relative to project root.
+      plugins: config?.plugins?.map((plugin: string) =>
+        plugin.startsWith('.') ? path.join(path.relative(root, configDir), plugin) : plugin,
+      ),
+      // Makes post globs absolute.
+      include: config?.include?.map((glob: string) => path.join(configDir, glob)),
+      exclude: config?.exclude?.map((glob: string) => path.join(configDir, glob)),
+    };
+  });
