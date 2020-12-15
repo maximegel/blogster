98e51ce2e62c04c836ed980443ab18bab9e93194
Maxime Gélinas <mgelinas@softframe.net>
2020-12-14 20:08:41 -0500

    fix(core): handle missing cover image in unsplash processor

--- packages/core/src/posts/processors/unsplash/parametrize-cover-image.ts
+++ packages/core/src/posts/processors/unsplash/parametrize-cover-image.ts
@@ -2,7 +2,7 @@ import { createMetadataProcessor, PostProcessor } from '../../post-processor';
 
 export const parametrizeCoverImage = (options: { width: number; height: number }): PostProcessor =>
   createMetadataProcessor(metadata => {
-    const [, unsplashId] = metadata.coverImage?.match(/^https?:\/\/unsplash\.com\/photos\/(\w+)$/i);
+    const [, unsplashId] = metadata.coverImage?.match(/^https?:\/\/unsplash\.com\/photos\/(\w+)$/i) ?? [];
     if (!unsplashId) return metadata;
     return { ...metadata, coverImage: `https://source.unsplash.com/${unsplashId}/${options.width}x${options.height}` };
   });
